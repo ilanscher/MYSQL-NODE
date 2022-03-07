85d0f24c39ff9cfc4711e4be603536b02d0d5fd4
ilanscher <ilan@nodeside.com>
2022-03-07 15:26:23 +0200

    test project

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.static(__dirname + '/public'));
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+  });
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
